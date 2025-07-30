e4851883e2c340cdf1af3b7aa26cf32a54ec8902
Léo-Nils Boissier <leo-nils.boissier@epigrapho.fr>
2025-07-30 07:52:31 +0200

    Make occupied space more even in the box tree

--- src/components/parse_tree_component.rs
+++ src/components/parse_tree_component.rs
@@ -13,21 +13,25 @@ pub fn ParseTreeComponent(
     tree: OwnedParseTree,
     production_hues: Arc<HashMap<String, f32>>,
 ) -> impl IntoView {
+    let weight = tree.weight;
     let lhs_name = match tree.lhs {
         Term::Terminal(name) => name,
         Term::Nonterminal(name) => name,
     };
 
-    let get_color = |name: &str, production_hues: Arc<HashMap<String, f32>>| -> String {
+    let get_style = |name: &str, production_hues: Arc<HashMap<String, f32>>| -> String {
         let hue = production_hues
             .get(&format!("<{}>", name))
             .unwrap_or(&0.0)
             .to_owned();
-        format!("background-color: hsl({}deg, 16%, 50%)", hue)
+        format!(
+            "flex: {}; background-color: hsl({}deg, 16%, 50%)",
+            weight, hue
+        )
     };
 
     view! {
-        <div class=style::non_terminal_node style=get_color(&lhs_name, production_hues.clone())>
+        <div class=style::non_terminal_node style=get_style(&lhs_name, production_hues.clone())>
             <div class=style::terminal_lhs>
                 {lhs_name.clone()}
             </div>
--- src/owned_tree.rs
+++ src/owned_tree.rs
@@ -14,21 +14,34 @@ impl OwnedParseTreeNode {
             }
         }
     }
+
+    pub fn get_weight(&self) -> u32 {
+        if let OwnedParseTreeNode::Nonterminal(tree) = self {
+            tree.weight
+        } else {
+            1
+        }
+    }
 }
 
 pub struct OwnedParseTree {
+    pub weight: u32,
     pub lhs: Term,
     pub rhs: Vec<OwnedParseTreeNode>,
 }
 
 impl OwnedParseTree {
     pub fn from_parse_tree(ref_tree: &ParseTree) -> Self {
+        let rhs: Vec<_> = ref_tree
+            .rhs_iter()
+            .map(OwnedParseTreeNode::from_parse_tree_node)
+            .collect();
+        let weight = rhs.iter().map(|x| x.get_weight()).sum();
+
         OwnedParseTree {
             lhs: ref_tree.lhs.to_owned(),
-            rhs: ref_tree
-                .rhs_iter()
-                .map(OwnedParseTreeNode::from_parse_tree_node)
-                .collect(),
+            rhs,
+            weight,
         }
     }
 }
